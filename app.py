ba1aae72a8fd080ab2d79b6c95dd6860c0583d9e
eurekesh <asdu8905@colorado.edu>
2023-04-01 14:30:25 -0600

    Add gunicorn

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+import os
+
 from flask import Flask
 from flask_opensearch import FlaskOpenSearch
 
 app = Flask(__name__)
+
+app.config["OPENSEARCH_HOST"] = os.environ.get("HOST")
+app.config["OPENSEARCH_USER"] = os.environ.get("USERNAME")
+app.config["OPENSEARCH_PASSWORD"] = os.environ.get("OS_PASSWD")
+
+opensearch = FlaskOpenSearch(
+    app=app,
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False
+)
 
 
 @app.route('/')
