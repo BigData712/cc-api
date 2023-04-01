9d0b5ce22f487bc9f6e000d64a8bf36fddb67427
eurekesh <asdu8905@colorado.edu>
2023-04-01 17:36:26 -0600

    CORS :(

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-import json
 import os
 from dotenv import load_dotenv
-from flask import Flask, jsonify, Response
+from flask import Flask, Response
+from flask_cors import CORS, cross_origin
 import requests
 
 load_dotenv()
 
 app = Flask(__name__)
+cors = CORS(app)
 
 host_name = os.environ.get("HOST")
 username = os.environ.get("USERNAME")
@@ -17,6 +18,7 @@ auth = (username, password)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:url>')
+@cross_origin()
 def process(url):
     res = requests.get(host_name + f"/{url}", auth=auth)
     return Response(res.text, mimetype='application/json')
