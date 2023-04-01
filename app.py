0f6a97a4130960fdceb043d4c894a058ec8df4f9
eurekesh <asdu8905@colorado.edu>
2023-04-01 16:18:52 -0600

    just doing passthrough, all done

--- app.py
+++ app.py
@@ -1,39 +1,24 @@
 import os
+from dotenv import load_dotenv
+from flask import Flask
+import requests
 
-from flask import Flask, request
-from flask_opensearch import FlaskOpenSearch
-from opensearchpy.client.cat import CatClient
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config["OPENSEARCH_HOST"] = os.environ.get("HOST")
-app.config["OPENSEARCH_USER"] = os.environ.get("USERNAME")
-app.config["OPENSEARCH_PASSWORD"] = os.environ.get("OS_PASSWD")
+host_name = os.environ.get("HOST")
+username = os.environ.get("USERNAME")
+password = os.environ.get("OS_PASSWD")
 
-opensearch = FlaskOpenSearch(
-    app=app,
-    use_ssl=True,
-    verify_certs=False,
-    ssl_assert_hostname=False,
-    ssl_show_warn=False
-)
+auth = (username, password)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-# issue a query on an index
-@app.route('/<index>', methods=['POST'])
-def process_query():
-    jayson = request.json
-
-@app.route('/indices')
-def get_indicides():
-    cat_client = CatClient(opensearch)
-
-    return cat_client.indices(format="json")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:url>')
+def process(url):
+    res = requests.get(host_name + f"/{url}", auth=auth)
+    return res.text
 
 
 if __name__ == '__main__':
