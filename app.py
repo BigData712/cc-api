37006e45b3381094e3810475afd76e8296c6b01b
eurekesh <asdu8905@colorado.edu>
2023-04-03 19:46:55 -0600

    New SQL capabilities! And .env example

--- app.py
+++ app.py
@@ -9,23 +9,31 @@ load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 
-host_name = os.environ.get("HOST")
-username = os.environ.get("USERNAME")
-password = os.environ.get("OS_PASSWD")
-
-auth = (username, password)
+HOSTNAME = os.environ.get("HOST")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("OS_PASSWD")
+AUTH = (USERNAME, PASSWORD)
+SQL_ENDPOINT = HOSTNAME + '/_plugins/_sql?format=json'
 
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:url>')
+@app.route('/<path:url>', methods=['GET', 'POST'])
 @cross_origin()
 def process(url):
-    args = request.query_string
-    formed_str = host_name + f"/{url}?{str(args, 'utf-8')}"
-    print(formed_str)
+    if request.method == 'GET':
+        args = request.query_string
+        formed_endpoint = HOSTNAME + f"/{url}?{str(args, 'utf-8')}"
+        print(formed_endpoint)
 
-    res = requests.get(formed_str, auth=auth)
-    return Response(res.text, mimetype='application/json')
+        res = requests.get(formed_endpoint, auth=AUTH)
+
+        return Response(res.text, mimetype='application/json')
+    elif url == 'sql' and request.method == 'POST':
+        query = request.json
+        print(query)
+
+        res = requests.post(SQL_ENDPOINT, auth=AUTH, json=query)
+        return Response(res.text, mimetype='application/json')
 
 
 if __name__ == '__main__':
