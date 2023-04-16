4c5db4309db64a695b02258c247ed8abd0885af3
eurekesh <asdu8905@colorado.edu>
2023-04-15 21:09:00 -0600

    Updated to use Redis, hopefully will fix latency issues!

--- app.py
+++ app.py
@@ -2,18 +2,33 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, Response, request
 from flask_cors import CORS, cross_origin
+from flask_caching import Cache
 import requests
 
 load_dotenv()
 
-app = Flask(__name__)
-cors = CORS(app)
+cache_config = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_REDIS_URL': os.environ.get('REDIS_URL'),
+    'CACHE_KEY_PREFIX': 'cc_api_'
+}
 
 HOSTNAME = os.environ.get("HOST")
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("OS_PASSWD")
 AUTH = (USERNAME, PASSWORD)
 SQL_ENDPOINT = HOSTNAME + '/_plugins/_sql?format=json'
+
+app = Flask(__name__)
+cors = CORS(app)
+cache = Cache(app, config=cache_config)
+
+
+@cache.memoize()
+def run_post_request(query: str) -> Response:
+    formed_query = dict()
+    formed_query['query'] = query
+    return requests.post(SQL_ENDPOINT, auth=AUTH, json=formed_query)
 
 
 @app.route('/', defaults={'path': ''})
@@ -29,10 +44,10 @@ def process(url):
 
         return Response(res.text, mimetype='application/json')
     elif url == 'sql' and request.method == 'POST':
-        query = request.json
+        query = request.json.get('query')
         print(query)
 
-        res = requests.post(SQL_ENDPOINT, auth=AUTH, json=query)
+        res = run_post_request(query)
         return Response(res.text, mimetype='application/json')
 
 
